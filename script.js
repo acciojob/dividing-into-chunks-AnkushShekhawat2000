afb48143d603fb5c3d4fc23b897d9276f3069964
ANKUSH SHEKHAWAT <114976201+AnkushShekhawat2000@users.noreply.github.com>
2023-12-05 09:51:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-const arr = [1, 2, 3, 4, 1, 0, 2, 2];
+const extendHex = (shortHex) => {
+  // Remove the # (if present) and convert to lowercase
+  shortHex = shortHex.replace("#", "").toLowerCase();
 
-const divide = (arr, n) => {
-  // Write your code here
+  // Check if the input is a valid short hex code
+  if (/^[0-9a-f]{3}$/.test(shortHex)) {
+    // Convert each short component to a full component
+    const fullHex = shortHex
+      .split("")
+      .map((char) => char.repeat(2))
+      .join("");
+
+    // Add the # prefix to the full hex code
+    return "#" + fullHex;
+  } else {
+    // Handle invalid input
+    alert("Invalid short hex code.");
+  }
 };
 
-const n = prompt("Enter n: ");
-alert(JSON.stringify(divide(arr, n)));
+// Do not change the code below.
+const shortHex = prompt("Enter Short Hex.");
+alert(extendHex(shortHex));
